fee060eebaf295d24f34947cedbd969eeb3e1cc6
nguyentn298 <kid2981@gmail.com>
2016-09-01 15:52:43 +0700

    First commit test

--- src/com/dante/test/TestDAO.java
+++ src/com/dante/test/TestDAO.java
@@ -4,5 +4,6 @@ public class TestDAO {
 	public static void main(String[] args) {
 		System.out.println("Test ti do ma");
 		System.out.println("Test ti do ma 222");
+		System.out.println("Test ti do ma 333");
 	}
 }
